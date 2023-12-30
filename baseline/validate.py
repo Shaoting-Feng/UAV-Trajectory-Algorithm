bb221bd89578fddc0c1850d0ac1c27ea0a0c04c0
Shaoting <fengshaoting@sjtu.edu.cn>
2023-12-30 08:22:34 +0100

    Delete baseline directory

--- baseline/validate.py
+++
@@ -1,5 +0,0 @@
-from stable_baselines3.common.env_checker import check_env
-from baseline.uav import UavTrajectory
-env = UavTrajectory()
-# If the environment don't follow the interface, an error will be thrown
-check_env(env, warn=True)
